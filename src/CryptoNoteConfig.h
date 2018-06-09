4db299ee911d41a8e316f9965b62a0725bdbb46a
nexgencomm <40043788+nexgencomm@users.noreply.github.com>
2018-06-09 06:55:40 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -132,7 +132,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 const char* const SEED_NODES[] = { 
   "server1.recyclebit.org:32347",
-  "server1.recyclebit.org:32347",
+  "server2.recyclebit.org:32347",
 };
 
 struct CheckpointData {
@@ -141,7 +141,7 @@ struct CheckpointData {
 };
 
 const std::initializer_list<CheckpointData> CHECKPOINTS = { 
-  {0,	"0x010a01ff0001fac484c69cd608029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f904925cc23f86f9f3565188862275dc556a9bdfb6aec22c5aca7f0177c45ba8" },
+  {0,	"0x" },
   
 };
 
