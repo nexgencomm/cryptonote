ad8db9f089b5de9bc424f46fc1c77191e76ff9a1
nexgencomm <40043788+nexgencomm@users.noreply.github.com>
2018-06-09 06:00:21 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -141,7 +141,7 @@ struct CheckpointData {
 };
 
 const std::initializer_list<CheckpointData> CHECKPOINTS = { 
-  {0,	"0x" },
+  {0,	"0x010a01ff0001fac484c69cd608029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f904925cc23f86f9f3565188862275dc556a9bdfb6aec22c5aca7f0177c45ba8" },
   
 };
 
